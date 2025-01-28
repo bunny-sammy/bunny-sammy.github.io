aa69feb4dece8624eda2661daa56f363f9f30e57
bunnysammy_ <davidenacrene@gmail.com>
2025-01-28 20:14:58 -0300

    :wrench: Importação de SVGs

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./i18n";
-import './styles/index.scss'
+import './styles/_index.scss'
 import App from './pages/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,28 +2,14 @@ import { useTranslation } from "react-i18next";
 import { useState } from 'react'
 
 import '../styles/App.scss'
+import Landing from "../components/Landing";
 
 export default function App() {
   const { i18n, t } = useTranslation();
 
   return (
     <main>
-      <div>
-        <img></img>
-        <h1>David Coelho</h1>
-        <h2>{t('occupation')}</h2>
-        <ul>
-          <li>
-            Linkedin
-          </li>
-          <li>
-            Github
-          </li>
-          <li>
-            Instagram
-          </li>
-        </ul>
-      </div>
+      <Landing/>
     </main>
   )
 }
