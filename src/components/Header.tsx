a76e8647dd48357878b8ab73a100a574b6b4e25c
bunnysammy_ <davidenacrene@gmail.com>
2025-03-21 09:45:17 -0300

    :lipstick: feat: Melhorias nos estilos da landing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,9 +63,9 @@ export default memo(function Header () {
         "--final-height": `${finalHeight}px`,
 
         "--user-opacity": modifier > 0.75 ? modifier : 0,
-        "--user-transform": `translate(${width * (0.5 * (1-modifier)) - 65*(1-modifier)}px, ${10 * (1-modifier)}px)`,
+        "--user-transform": `translate(${width * (0.25 * (1-modifier)) - 65*(1-modifier)}px, ${10 * (1-modifier)}px)`,
+        "--name-transform": `scale(${Math.max(0.6, 1-modifier)}, ${Math.max(0.6, 1-modifier)}) translate(-${modifier > 0.01 ? (width * (0.2 * modifier)) : 0}px, -${modifier > 0.01 ? (100 * modifier) : 0}px)`,
         "--pic-transform": `scale(${Math.max(0.2, 1-modifier)}, ${Math.max(0.2, 1-modifier)})`,
-        "--name-transform": `scale(${Math.max(0.6, 1-modifier)}, ${Math.max(0.6, 1-modifier)}) translate(-${modifier > 0.01 ? (width * (0.5 * modifier) + 90*modifier) : 0}px, 0)`,
         "--scroll-display": modifier > 0.9 ? 'none' : 'block',
         "--pointer-events": modifier >= 0.9 ? 'none' : 'auto',
     } as React.CSSProperties;
